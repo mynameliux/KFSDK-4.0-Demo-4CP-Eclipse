52193cb35130073d8434734a9c9f2c868c71ac7b
liuxue <liuxue@kuaifazs.com>
2017-06-08 17:35:40 +0800

    Update so and MainActivity

--- KFSDK4EclipseDemo/src/com/kfsdk/eclipse/demo/MainActivity.java
+++ KFSDK4EclipseDemo/src/com/kfsdk/eclipse/demo/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -53,6 +54,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         initKFSDK();
         refreshBaseInfo();
     }
+
 
     private void refreshBaseInfo() {
         tvMainBaseInfo.setText(getDemoBaseInfo());
@@ -377,4 +379,5 @@ public class MainActivity extends Activity implements View.OnClickListener {
         String resStr = "code: " + code + " |msg: " + msg;
         tvMainResult.setText(resStr);
     }
+
 }
